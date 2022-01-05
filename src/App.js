731a33426bda0b63115a93b0dd39403b103fe7de
Alisa <alisa@jonnix.co>
2022-01-05 21:56:41 +0200

    App:: added logic for the scroll and added comment component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import Comment from "./components/Comment";
+import { useIntersectionObserver } from "./hooks/useIntersectionObserver";
+import { LoadingContainer } from "./styles/global";
 
-function App() {
+const commentsLimit = 20;
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const ref = useRef(null);
+
+  const isBottomVisible = useIntersectionObserver(
+    ref,
+    {
+      threshold: 0
+    },
+    false
+  );
+
+  useEffect(() => {
+    isBottomVisible && setCount(count + 1);
+  }, [isBottomVisible, count]);
+
   return (
     <div>
-      
+      {(() => {
+        const comments = [];
+
+        for (let i = 1; i <= count * commentsLimit; i++) {
+          comments.push(<Comment key={i} id={i} />);
+        }
+
+        return comments;
+      })()}
+      <LoadingContainer ref={ref}>
+        Loading...
+      </LoadingContainer>
     </div>
   );
 }
